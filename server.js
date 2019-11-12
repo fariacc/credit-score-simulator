43c0d730371b1137bbc87552a8ef0ada767e22b1
fariacc <carolinacfaria94@hotmail.com>
2019-11-12 02:36:06 +0100

    First commit with the application

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5001;
 app.listen(port);
